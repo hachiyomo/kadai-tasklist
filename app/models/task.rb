da1e157eb991c953d2e8586090457d09883d69a8
hachiyomo <yomohachi09@gmail.com>
2022-01-03 15:44:10 +0000

    user login

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+  
+  # belongs_to :users
+  
   validates :status, presence: true, length: { maximum: 10 }
   validates :content, presence: true, length: { maximum: 255 }
+  
 end
