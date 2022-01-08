ef36de941e008502dd19b14e22d3a6178239e9c7
hachiyomo <yomohachi09@gmail.com>
2022-01-08 11:57:23 +0000

    tasklist_3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  # before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = current_user.tasks.where(id = params[:id])
+    @tasks = current_user.tasks
+    # .where(id = params[:id])
   end
   
   def create
@@ -50,9 +51,9 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  # def set_task
+  #   @task = Task.find(params[:id])
+  # end
   
   def task_params
     params.require(:task).permit(:content, :status)
