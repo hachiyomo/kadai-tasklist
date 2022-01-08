178c98bbacaac055affd4f8b4edde260f42fa96e
hachiyomo <yomohachi09@gmail.com>
2022-01-08 04:11:35 +0000

    tasklist-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_064534) do
+ActiveRecord::Schema.define(version: 2022_01_08_040118) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
-    t.text "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_01_02_064534) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "tasks", "users"
